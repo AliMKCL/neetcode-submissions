454a0c78e049af4d3a5b08eea0a9466d78638b79
Ali Murat Keçeli <a.muratkeceli@gmail.com>
2026-09-15 14:20:34 +0300

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # Fixed-size arrays are O(1).
+
+        len1, len2 = len(s1), len(s2)
+        if len1 > len2:
+            return False
+
+        count1 = [0] * 26
+        count2 = [0] * 26
+
+        for i in range(len1):
+            count1[ord(s1[i]) - ord("a")] += 1
+            count2[ord(s2[i]) - ord("a")] += 1
+
+        if count1 == count2:
+                return True
+
+        for i in range(len1, len2):
+            count2[ord(s2[i-len(s1)]) - ord("a")] -= 1 # Delete leftmost character
+            count2[ord(s2[i]) - ord("a")] += 1 # Add new character from right
+
+            if count1 == count2:
+                return True
+        
+        return False
